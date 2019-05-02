a2140f0e78a5f871552b712a3b969efb9f0e9ac9
mes32 <stockman.mike@gmail.com>
2019-05-02 10:24:31 -0500

    Build special handling for timestamps in class Message.

--- server/modules/Message.js
+++ server/modules/Message.js
@@ -1,13 +1,34 @@
 const moment = require('moment');
-const momentTimezone = require('moment-timezone');
+require('moment-timezone');
+
+const INPUT_UNIX_SECONDS = 'x';
+const MESSAGE_TIME_FORMAT = 'ddd DD/MM/YYYY, h:mmA z';
 
 class Message {
     constructor(template, company, guest) {
-        const timezone = company.timezone;
-        const currentTime = moment().tz(timezone);
-
+        const currentTime = moment().tz(company.timezone);
+        let message = this.evaluateVariables(template.message, company, guest, currentTime);
+        
         this.timestamp = currentTime.format();
-        this.message = template.message;
+        this.message = message;
+    }
+
+    evaluateVariables(template, company, guest, currentTime) {
+        const timezone = company.timezone;
+        const startReservation = this.getTimeFormat(guest.reservation.startTimestamp, timezone);
+        const endReservation = this.getTimeFormat(guest.reservation.endTimestamp, timezone);
+
+        let message = startReservation + ' thru ' + endReservation;
+
+        return message;
+    }
+
+    getTimeFormat(timestamp, timezone) {
+        if (timestamp && timezone) {
+            return moment(timestamp, INPUT_UNIX_SECONDS).tz(timezone).format(MESSAGE_TIME_FORMAT);
+        } else {
+            throw new Error('Invalid timestamp or timezone in method getTimeFormat() of class Message.');
+        }
     }
 }
 
